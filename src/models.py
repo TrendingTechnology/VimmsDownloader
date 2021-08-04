2907a3e50a007d0f9f527abb0ff9217c84946544
Brian <brian.p.tipton@gmail.com>
2021-08-04 08:27:34 +0000

    added comments and docstrings on classes

--- src/models.py
+++ src/models.py
@@ -3,6 +3,8 @@ from typing import List
 
 
 class Config:
+    """Config object created based on user input"""
+
     def __init__(self,
                  Selections: List[str] = [],
                  All: bool = False,
@@ -17,24 +19,34 @@ class Config:
 
 
 class ROM():
+    """Used to hold a details about a single ROM"""
+
     def __init__(self, Name: str, URI: str):
         self.Name = Name
         self.URI = URI
 
+# Sections are either a search query or #-A-Z subsections on vimms
+
 
 class SectionofROMs:
+    """Used to hold a details about multiple ROMs"""
+
     def __init__(self, Section: str, ROMS: List[ROM]):
         self.Section = Section
         self.ROMS = ROMS
 
 
 class SearchSelection:
+    """Used when in search mode to get the parameters for the search criteria"""
+
     def __init__(self, System: str = '', Query: str = ''):
         self.System = System
         self.Query = Query
 
 
 class BulkSystemROMS:
+    """Used hold all of a systems ROMS"""
+
     def __init__(self, Sections: List[SectionofROMs], System: str):
         self.System = System
         self.Sections = Sections
